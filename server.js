cf6e4ae6f20368b9529705e90ebfde8c8df4f749
Radhey Chitroda <radheychitroda@gmail.com>
2024-06-04 23:47:19 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const sql = require("@vercel/postgres");
 const app = express();
 let tasks = [];
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+app.get("/", async (req, res) => {
+  try {
+    const result =
+      await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
+      console.log("result = " + result);
+    return res.send("Express on Vercel");
+  } catch (error) {
+    return res.send("Express on Vercel ok" + error);
+  }
 });
 
 app.listen(5000, () => {
@@ -17,11 +25,10 @@ app.post("/add", (req, res) => {
     res.status(400).json({
       data: "task name required",
     });
-  }
-  else if(req.body.task.trim() == "") {
+  } else if (req.body.task.trim() == "") {
     res.status(400).json({
-        data: "enter proper task name",
-      });
+      data: "enter proper task name",
+    });
   }
   res.status(200).json({
     data: "User added successfully",
